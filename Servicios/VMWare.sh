9159fd4ca8e6b9b36adaab58ffbbe91109c72a3f
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-11 15:01:37 +0100

    Update VMWare.sh

--- Servicios/VMWare.sh
+++ Servicios/VMWare.sh
@@ -13,3 +13,13 @@ mhwd-kernel -l
 
 #Herramientas GUI Kernels
 manjaro-settings-manager -m msm_kernel
+
+
+
+
+git clone -b workstation-15.1.0 https://github.com/mkubecek/vmware-host-modules.git
+cd vmware-host-modules/
+tar -cf vmmon.tar vmmon-only
+tar -cf vmnet.tar vmnet-only
+cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/
+vmware-modconfig --console --install-all
