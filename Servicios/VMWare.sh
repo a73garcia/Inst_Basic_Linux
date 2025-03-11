060657ff35019d7e9465607e02ab7f4898c828c7
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-11 14:45:29 +0100

    Update VMWare.sh

--- Servicios/VMWare.sh
+++ Servicios/VMWare.sh
@@ -1,1 +1,15 @@
 #VMWare
+
+
+
+
+pacman -S linux-headers
+
+#VMWare-Keymaps
+https://aur.archlinux.org/vmware-keymaps.git 
+
+mhwd-kernel -l
+
+
+#Herramientas GUI Kernels
+manjaro-settings-manager -m msm_kernel
