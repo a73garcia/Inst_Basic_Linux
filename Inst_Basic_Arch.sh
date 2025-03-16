be3f6606684c4bcde8eb73cf3553603cf94dfa8a
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-16 14:25:40 +0100

    Update Inst_Basic_Arch.sh

--- Inst_Basic_Arch.sh
+++ Inst_Basic_Arch.sh
@@ -22,10 +22,10 @@ sudo pacman -S fuse2 gtkmm linux-headers pcsclite libcanberra
 paru -S --noconfirm --needed ncurses5-compat-libs
 paru -S --noconfirm --needed  vmware-workstation
 
-sudo systemctl enable vmware-networks.service  vmware-usbarbitrator.service vmware-networks-configuration.service
-sudo systemctl start vmware-networks.service  vmware-usbarbitrator.service vmware-networks-configuration.service
+sudo systemctl enable vmware-networks.service vmware-usbarbitrator.service vmware-networks-configuration.service
+sudo systemctl start vmware-networks.service vmware-usbarbitrator.service vmware-networks-configuration.service
 
-sudo systemctl status vmware-networks.service  vmware-usbarbitrator.service 
+sudo systemctl status vmware-networks.service vmware-usbarbitrator.service vmware-networks-configuration.service
 
 #vmware-networks-configuration.service first to generate /etc/vmware/networking
 #vmware-networks.service for guest network access (otherwise you will get an error could no connect 'ethernet 0' to virtual network and you will not be able to use vmware-netcfg)
