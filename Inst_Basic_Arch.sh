2f38612cccc28a2c3c0023731c5acb58031f7d31
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-16 13:11:05 +0100

    Update Inst_Basic_Arch.sh

--- Inst_Basic_Arch.sh
+++ Inst_Basic_Arch.sh
@@ -13,38 +13,26 @@ sudo pacman -Syuu
 pamac install base-devel
 
 # Instalar VMWare
-#echo -e "\033[31mInstalando VMWare...\033[0m"
-#cd /tmp/
-#sudo pacman -S dkms gtkmm3 libxcrypt-compat
-#git clone https://aur.archlinux.org/vmware-keymaps.git 
-#cd vmware-keymaps/
-#makepkg -si
-#cd..
-#git clone https://aur.archlinux.org/vmware-workstation.git 
-#cd vmware-workstation/
-#makepkg -si
 
-#first to generate /etc/vmware/networking
-#sudo systemctl start vmware-networks-configuration.service
-#sudo systemctl enable vmware-networks-configuration.service
-#sudo systemctl status vmware-networks-configuration.service
+cd /tmp/
+git clone https://aur.archlinux.org/paru-bin
+cd paru-bin/
+makepkg -si
 
-#for guest network access (otherwise you will get an error could no connect 'ethernet 0' to virtual network and you will not be able to use vmware-netcfg)
-#sudo systemctl start vmware-networks.service
-#sudo systemctl enable vmware-networks.service
-#sudo systemctl status vmware-networks.service
+sudo pacman -S fuse2 gtkmm linux-headers pcsclite libcanberra
+paru -S --noconfirm --needed ncurses5-compat-libs
+paru -S --noconfirm --needed  vmware-workstation
 
-#for connecting USB devices to guest
-#sudo systemctl start vmware-usbarbitrator.service
-#sudo systemctl enable vmware-usbarbitrator.service
-#sudo systemctl status vmware-usbarbitrator.service
+sudo systemctl enable vmware-networks.service  vmware-usbarbitrator.service vmware-networks-configuration.service
+sudo systemctl start vmware-networks.service  vmware-usbarbitrator.service vmware-networks-configuration.service
 
-#for sharing virtual machines (not available since version 16)
-#sudo systemctl start vmware-hostd.service
-#sudo systemctl enable vmware-hostd.service
-#sudo systemctl status vmware-hostd.service
+sudo systemctl status vmware-networks.service  vmware-usbarbitrator.service 
 
 sudo modprobe -a vmw_vmci vmmon
+
+#vmware-networks-configuration.service first to generate /etc/vmware/networking
+#vmware-networks.service for guest network access (otherwise you will get an error could no connect 'ethernet 0' to virtual network and you will not be able to use vmware-netcfg)
+#vmware-usbarbitrator.service for connecting USB devices to guest
 
 # Instalar paquetes necesarios
 echo -e "\033[31mInstalando paquetes...\033[0m"
