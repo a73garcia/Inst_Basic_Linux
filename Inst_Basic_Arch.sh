6632134c959f31fe6ecd49d5fd980ef98e43d705
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-24 13:34:31 +0100

    Update Inst_Basic_Arch.sh

--- Inst_Basic_Arch.sh
+++ Inst_Basic_Arch.sh
@@ -14,7 +14,7 @@ pamac install base-devel
 
 # Instalar paquetes necesarios
 echo -e "\033[31mInstalando paquetes...\033[0m"
-sudo pacman -S git wget kate feh p7zip locate yay lsd bat net-tools neovim gparted curl fastfetch kitty dolphin gimp inkscape hexchat qbittorrent putty thunderbird keepassxc texlive texmaker powerline-fonts gnome-boxes zsh vscode yt-dlp gdisk
+sudo pacman -S git wget kate feh p7zip locate yay lsd bat net-tools neovim gparted curl fastfetch kitty dolphin gimp inkscape hexchat qbittorrent putty thunderbird keepassxc texlive texmaker powerline-fonts gnome-boxes zsh vscode yt-dlp gdisk testdisk
 
 # Instalar Media Writer
 echo -e "\033[31mInstalando MediaWriter...\033[0m"
@@ -121,7 +121,8 @@ echo -e "
             p7zip           fastfetch           thunderbird         gdisk
             locate          kitty               keepass             zsh
             lsd             dolphin             keepassxc           MediaWriter
-            bat             gimp                texlive             yt-dlp\033[0m
+            bat             gimp                texlive             yt-dlp
+            testdisk\033[0m
 
 - Se ha verificado actualizaciones
 - Se ha cambiado la Shell a ZSH para el Usuario y Root
