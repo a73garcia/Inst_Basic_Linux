2d8081b47a06203c5be3961e9cbfbb4e13a09056
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-25 15:50:35 +0100

    Update NordVPN.sh

--- Servicios/NordVPN.sh
+++ Servicios/NordVPN.sh
@@ -3,3 +3,16 @@
 # https://snapcraft.io/nordvpn
 
 sudo snap install nordvpn
+
+
+sudo groupadd nordvpn
+sudo usermod -aG nordvpn $USER
+
+
+# https://support.nordvpn.com/hc/es/articles/20196094470929-Instala-NordVPN-en-distribuciones-Linux
+
+nordvpn login
+
+nordvpn connect
+
+nordvpn
