a66b08299acd8610f6b3f0a29f059aa451c46d54
a73garcia <153382653+a73garcia@users.noreply.github.com>
2025-03-25 20:55:32 +0100

    Update NordVPN.sh

--- Servicios/NordVPN.sh
+++ Servicios/NordVPN.sh
@@ -11,8 +11,36 @@ sudo usermod -aG nordvpn $USER
 
 # https://support.nordvpn.com/hc/es/articles/20196094470929-Instala-NordVPN-en-distribuciones-Linux
 
+
+sudo snap connect nordvpn:hardware-observe
+sudo snap connect nordvpn:network-control
+sudo snap connect nordvpn:network-observe
+sudo snap connect nordvpn:firewall-control
+sudo snap connect nordvpn:login-session-observe
+sudo snap connect nordvpn:system-observe
+
+
+
 nordvpn login
+
+# Entrar en https://api.nordvpn.com/v1/users/oauth/login-redirect?attempt=ec39ae4d-4e87-4c21-b1c8-f6815138030f
+# Loguearnos
+# Pulsar entro en la cosola
 
 nordvpn connect
 
-nordvpn
+# Ver el estado
+nordvpn status 
+
+# Ver la lista de países
+nordvpn countries
+
+# Ver la lista de ciudades
+nordvpn cities
+
+# lista de los grupos de servidores disponibles:
+nordvpn groups
+
+
+# https://geekland.eu/instalar-configurar-y-usar-nordvpn-en-linux-desde-la-terminal/
+# https://support.nordvpn.com/hc/es/articles/20196094470929-Instala-NordVPN-en-distribuciones-Linux
